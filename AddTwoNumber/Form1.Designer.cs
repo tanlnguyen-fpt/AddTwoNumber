284e3014ace5817121c7177cdd8400aa05eef2dc
Tan Nguyen <tannlde150056@fpt.edu.vn>
2022-02-22 19:06:58 +0700

    Add project files.

---
+++ AddTwoNumber/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace AddTwoNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Number1 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.Number2 = new System.Windows.Forms.Label();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Number1
+            // 
+            this.Number1.AutoSize = true;
+            this.Number1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Number1.Location = new System.Drawing.Point(119, 97);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(102, 25);
+            this.Number1.TabIndex = 0;
+            this.Number1.Text = "Number 1";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(256, 98);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(160, 23);
+            this.txtNumber1.TabIndex = 1;
+            this.txtNumber1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumber1_KeyDown);
+            // 
+            // Number2
+            // 
+            this.Number2.AutoSize = true;
+            this.Number2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Number2.Location = new System.Drawing.Point(119, 142);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(102, 25);
+            this.Number2.TabIndex = 0;
+            this.Number2.Text = "Number 2";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(256, 143);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(160, 23);
+            this.txtNumber2.TabIndex = 1;
+            this.txtNumber2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumber2_KeyDown);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Result.Location = new System.Drawing.Point(119, 189);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(66, 25);
+            this.Result.TabIndex = 0;
+            this.Result.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(256, 190);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(160, 23);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSum.Location = new System.Drawing.Point(96, 248);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(89, 40);
+            this.btnSum.TabIndex = 2;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.Location = new System.Drawing.Point(232, 248);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(89, 40);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.Location = new System.Drawing.Point(366, 248);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 40);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 327);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Number2);
+            this.Controls.Add(this.Number1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Number1;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label Number2;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ AddTwoNumber/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddTwoNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            double result;
+            double num1 = 0;
+            double num2 = 0;
+            try
+            {
+                num1 = double.Parse(txtNumber1.Text);
+                num2 = double.Parse(txtNumber2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter a double number\n" + ex.Message);
+            }
+            result = num1 + num2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtNumber1.Text = "";
+            txtNumber2.Text = "";
+            txtResult.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtNumber1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Tab)
+                txtNumber2.Focus();
+        }
+
+        private void txtNumber2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Tab)
+                btnSum_Click(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNumber1.Select();
+        }
+    }
+}
